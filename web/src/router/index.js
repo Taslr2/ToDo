d0213f4ccd327e2eda32f28aa629ed163f5a30f9
Taslr2 <19535838578@163.com>
2025-02-02 20:22:33 +0800

    更新HomePage路由

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
 
-      component: () => import('@/views/HomePage.vue'),
+      component: () => import('@/views/main/HomePage.vue'),
     },
   ],
 })
