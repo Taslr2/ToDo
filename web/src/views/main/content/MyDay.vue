42cf047914f8cf01fc5007da785ab8ffa93def61
Taslr2 <19535838578@163.com>
2025-02-03 21:19:07 +0800

    我的一天组件

---
+++ web/src/views/main/content/MyDay.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="test">我的一天</div>
+</template>
+
+<style scoped>
+.test {
+  width: 100%;
+  height: 100%;
+  background-color: blue;
+}
+</style>
