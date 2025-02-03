9bd63fdf22a8c9a96fe7c628fcaf3eb37a5b65ac
Taslr2 <19535838578@163.com>
2025-02-03 23:01:07 +0800

    主页缩略图标添加

--- web/src/components/sidebar/SideNavigation.vue
+++ web/src/components/sidebar/SideNavigation.vue
@@ -1,7 +1,20 @@
-<script setup></script>
+<script setup>
+const toggleMenu = () => {
+  console.log('缩略功能未完成')
+}
+</script>
 
 <template>
   <div class="total">
+    <div class="toggle" @click="toggleMenu">
+      <img
+        class="menu-icon"
+        src="@/assets/icons/menu-icon.svg"
+        alt="Menu Icon"
+        width="20"
+        height="20"
+      />
+    </div>
     <ul class="nav">
       <li><router-link to="/">我的一天</router-link></li>
       <li><router-link to="/inplan">计划内</router-link></li>
@@ -12,7 +25,7 @@
 
 <style scoped>
 .total {
-  width: 292px;
+  width: 290px;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -28,5 +41,18 @@
   width: 3px;
   height: 100%;
   background: linear-gradient(to right, #eee, #faf9f8);
+}
+
+.toggle {
+  display: flex;
+  width: 100%;
+  height: 48px;
+  box-sizing: border-box;
+  margin: 16px 0px 0px;
+  padding: 0px 24px;
+  align-items: center;
+}
+.menu-icon {
+  cursor: pointer;
 }
 </style>
