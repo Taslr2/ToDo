a2cac93147a2e9eee454f7fff9110dffdfd38e21
Taslr2 <19535838578@163.com>
2025-02-03 21:17:40 +0800

    编写主页路由

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -6,8 +6,23 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-
       component: () => import('@/views/main/HomePage.vue'),
+      children: [
+        {
+          path: '',
+          component: () => import('@/views/main/content/MyDay.vue'),
+        },
+        {
+          path: 'inplan',
+          name: 'InPlan',
+          component: () => import('@/views/main/content/InPlan.vue'),
+        },
+        {
+          path: 'alltasks',
+          name: 'AllTasks',
+          component: () => import('@/views/main/content/AllTasks.vue'),
+        },
+      ],
     },
   ],
 })
