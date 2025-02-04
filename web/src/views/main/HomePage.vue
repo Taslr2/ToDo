0455dd39f6211783d234a2f4af418a850df90734
Taslr2 <19535838578@163.com>
2025-02-04 21:43:45 +0800

    侧边栏隐藏功能添加

--- web/src/views/main/HomePage.vue
+++ web/src/views/main/HomePage.vue
@@ -1,13 +1,20 @@
 <script setup>
-import HeadNavigation from '@/components/HeadNavigation.vue'
-import SideNavigation from '@/components/sidebar/SideNavigation.vue'
+import HeadNavigation from '@/components/HeadNavigation.vue';
+import SideNavigation from '@/components/sidebar/SideNavigation.vue';
+import { ref } from 'vue';
+
+const isSidebarVisible = ref(true);
+
+const handleUpdateVisibility = (visibility) => {
+  isSidebarVisible.value = visibility;
+};
 </script>
 
 <template>
   <HeadNavigation />
   <div class="main-container">
-    <div class="sidebar">
-      <SideNavigation />
+    <div :class="['sidebar', { 'hidden-sidebar': !isSidebarVisible }]">
+      <SideNavigation @updateVisibility="handleUpdateVisibility" />
     </div>
     <div class="content">
       <RouterView />
@@ -16,10 +23,31 @@ import SideNavigation from '@/components/sidebar/SideNavigation.vue'
 </template>
 
 <style scoped>
+body,
+html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+}
+
 .main-container {
   display: flex;
   height: calc(100vh - 48px);
 }
+
+.sidebar {
+  width: 290px;
+  background-color: #fff;
+  transition: width 0.3s;
+}
+
+.hidden-sidebar {
+  width: 0;
+  overflow: hidden;
+}
+
 .content {
   width: 100%;
 }
