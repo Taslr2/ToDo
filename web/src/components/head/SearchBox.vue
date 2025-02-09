878c45b7a6e461359194e187add78962cc7bc01f
Taslr2 <19535838578@163.com>
2025-02-09 22:43:37 +0800

    搜索框解锁锁定功能增加

--- web/src/components/head/SearchBox.vue
+++ web/src/components/head/SearchBox.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue'
 
 const searchQuery = ref('')
-const isSearchBoxAvailable = ref(false)
 const isEditing = ref(false)
 
 const handleSearch = () => {
@@ -18,15 +17,9 @@ const stopEditing = () => {
   searchQuery.value = ''
 }
 
-const handleMouseDown = () => {
+const searchContent = () => {
   if (searchQuery.value === '') {
-    isSearchBoxAvailable.value = false
-  }
-}
-
-const handleMouseUp = () => {
-  if (searchQuery.value === '') {
-    isSearchBoxAvailable.value = true
+    isEditing.value = true
   } else {
     console.log('Search query:', searchQuery.value) // 搜索功能
   }
@@ -42,19 +35,20 @@ const handleMouseUp = () => {
       v-model="searchQuery"
       @keyup.enter="handleSearch"
       placeholder="搜索"
-      :disabled="isSearchBoxAvailable"
     >
       <template #prefix>
-        <i class="iconfont icon-sousuo"
-           @mousedown="handleMouseDown"
-           @mouseup="handleMouseUp"></i>
+        <span class="input-prefix" @click="searchContent">
+          <i class="iconfont icon-sousuo"></i>
+        </span>
       </template>
       <template #suffix>
-        <i class="iconfont icon-fork" @click="stopEditing"></i>
+        <span class="input-suffix" @click="stopEditing">
+          <i class="iconfont icon-fork"></i>
+        </span>
       </template>
     </el-input>
     <div v-else class="init-button" @click="startEditing">
-      <i class="iconfont icon-sousuo"></i>
+      <i class="iconfont icon-sousuo" style="margin-left: 10px"></i>
     </div>
   </div>
 </template>
@@ -67,15 +61,18 @@ const handleMouseUp = () => {
   width: 100%;
   font-size: 14px;
 }
-.el-input__wrapper {
+::v-deep .el-input__wrapper {
   background-color: #fff;
   border-radius: 5px;
+  height: 33.33px;
+  padding: 0;
 }
-
+::v-deep .el-input__inner {
+  height: 100%;
+}
 .init-button {
   width: 100%;
   height: 33.33px;
-  padding: 0 10px;
   font-size: 14px;
   border-radius: 5px;
   background-color: #fff;
@@ -87,13 +84,16 @@ const handleMouseUp = () => {
 .init-button:hover {
   background-color: #f5f7fa;
 }
-::v-deep .el-input__prefix,
-::v-deep .el-input__suffix {
-  width: 30px;
+.input-prefix,
+.input-suffix {
+  width: 40px;
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
+}
+.input-prefix i {
+  transform: translate(1px, 1px);
 }
 </style>
