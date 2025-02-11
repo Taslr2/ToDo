f72a6dc309c65b5677f8138dff8cb7f677f3555d
Taslr2 <19535838578@163.com>
2025-02-11 23:03:33 +0800

    完成个人信息页面跳转

---
+++ web/src/components/RightPopUpWindows/PersonalInformation.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+import rightCancel from '@/assets/svg/rightcancel.svg'
+const props = defineProps(['isPersonalVisible'])
+const emit = defineEmits(['closeIsPersonal'])
+const isPersonalVisible = props.isPersonalVisible
+
+const closeIsPersonal = () => {
+  emit('closeIsPersonal', false)
+}
+</script>
+<template>
+  <transition>
+    <div class="total" v-show="isPersonalVisible">
+      <div class="title">
+        <h3>个人主页</h3>
+        <button><img :src="rightCancel" @click="closeIsPersonal" /></button>
+      </div>
+    </div>
+  </transition>
+</template>
+<style scoped>
+.total {
+  width: 350px;
+  height: 100%;
+  background-color: #faf9f8;
+}
+.title {
+  width: 100%;
+  height: 58px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #e5e5e5;
+}
+</style>
