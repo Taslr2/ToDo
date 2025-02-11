50386f22cab6d82a6be0c79927faf4a2f912f6e8
Taslr2 <19535838578@163.com>
2025-02-11 23:03:14 +0800

    完成帮助页面跳转

---
+++ web/src/components/RightPopUpWindows/HelpWindow.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+import rightCancel from '@/assets/svg/rightcancel.svg'
+const props = defineProps(['isHelpVisible'])
+const emit = defineEmits(['closeIsHelp'])
+const isHelpVisible = props.isHelpVisible
+
+const closeIsHelp = () => {
+  emit('closeIsHelp', false)
+}
+</script>
+<template>
+  <transition>
+    <div class="total" v-show="isHelpVisible">
+      <div class="title">
+        <h3>帮助</h3>
+        <button><img :src="rightCancel" @click="closeIsHelp" /></button>
+      </div>
+    </div>
+  </transition>
+</template>
+<style scoped>
+.total {
+  width: 350px;
+  height: 100%;
+  background-color: #faf9f8;
+}
+.title {
+  width: 100%;
+  height: 58px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #e5e5e5;
+}
+</style>
