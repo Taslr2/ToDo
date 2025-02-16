dcdb7af13ddf1fc4ce8371ccda7154b227cb6b71
Taslr2 <19535838578@163.com>
2025-02-16 23:40:49 +0800

    完成pinia状态管理书写

--- web/src/stores/sidebar.js
+++ web/src/stores/sidebar.js
@@ -1,59 +1,53 @@
-// import { ref, computed } from 'vue'
-// import { defineStore } from 'pinia'
-
-// export const useCounterStore = defineStore('counter', () => {
-//   const count = ref(0)
-//   const doubleCount = computed(() => count.value * 2)
-//   function increment() {
-//     count.value++
-//   }
-
-//   return { count, doubleCount, increment }
-// })
-import { ref } from 'vue'
 import { defineStore } from 'pinia'
-
 import calendarIcon from '@/assets/svg/calendar.svg'
 import sunIcon from '@/assets/svg/sun.svg'
 import houseIcon from '@/assets/svg/house.svg'
 import bellIcon from '@/assets/svg/bell.svg'
 
-const [isanalysisvisible, istaskvisible, iscalendarvisible, isfourquadrantvisible] = [
-  ref(true),
-  ref(true),
-  ref(true),
-  ref(true),
-]
-export const useSideStore = defineStore('counter', () => {
-  const menuItems = [
-    {
-      text: '日历',
-      iconClass: 'sun',
-      iconSrc: calendarIcon,
-      route: '/',
-      isVisible: iscalendarvisible,
+export const useSideStore = defineStore('sidebar', {
+  state: () => ({
+    selectedIndex: 0, 
+    isanalysisvisible: true,
+    istaskvisible: true,
+    iscalendarvisible: true,
+    isfourquadrantvisible: true,
+    menuItems: [
+      {
+        text: '日历',
+        iconClass: 'sun',
+        iconSrc: calendarIcon,
+        route: '/',
+        isVisible: true,
+      },
+      {
+        text: '四象限',
+        iconClass: 'calendar',
+        iconSrc: sunIcon,
+        route: '/fourquadrant',
+        isVisible: true,
+      },
+      {
+        text: '任务详情',
+        iconClass: 'house',
+        iconSrc: houseIcon,
+        route: '/taskdetails',
+        isVisible: true,
+      },
+      {
+        text: '统计分析',
+        iconClass: 'house',
+        iconSrc: bellIcon,
+        route: '/statisticalanalysis',
+        isVisible: true,
+      },
+    ],
+  }),
+  actions: {
+    initSelectedIndex(currentPath) {
+      const index = this.menuItems.findIndex((item) => item.route === currentPath)
+      if (index !== -1) {
+        this.selectedIndex = index
+      }
     },
-    {
-      text: '四象限',
-      iconClass: 'calendar',
-      iconSrc: sunIcon,
-      route: '/fourquadrant',
-      isVisible: isfourquadrantvisible,
-    },
-    {
-      text: '任务详情',
-      iconClass: 'house',
-      iconSrc: houseIcon,
-      route: '/taskdetails',
-      isVisible: istaskvisible,
-    },
-    {
-      text: '统计分析',
-      iconClass: 'house',
-      iconSrc: bellIcon,
-      route: '/statisticalanalysis',
-      isVisible: isanalysisvisible,
-    },
-  ]
-  return { menuItems, isanalysisvisible, istaskvisible, isfourquadrantvisible }
+  },
 })
