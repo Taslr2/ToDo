ee5cf9af828259b55a492fd0ba92c7a25011968b
GHOST <3089308393@qq.com>
2025-02-17 16:54:26 +0800

    taskListView部分代码改为后端对接

--- web/src/views/main/content/CalendarView/TaskListView.vue
+++ web/src/views/main/content/CalendarView/TaskListView.vue
@@ -2,7 +2,12 @@
   <div class="TaskListView">
     <div class="calendar-content">
       <transition-group name="bounce" tag="div">
-        <div v-for="(task, index) in filteredTasks" :key="index" class="task-card" @click="emitTask(task)">
+        <div
+          v-for="(task, index) in filteredTasks"
+          :key="index"
+          class="task-card"
+          @click="emitTask(task)"
+        >
           <div class="task-title">{{ task.title }}</div>
           <div class="task-category">{{ task.category }}</div>
           <div class="task-date">{{ task.expectedCompletionDate }}</div>
@@ -25,6 +30,7 @@
 
 <script setup>
 import { ref, defineProps, watch, defineEmits } from 'vue'
+import axios from 'axios'
 
 // 定义接收的 props
 const props = defineProps({
@@ -33,23 +39,36 @@ const props = defineProps({
   day: Number,
 })
 
-console.log("接收：", props.year, props.month, props.day)
+console.log('接收：', props.year, props.month, props.day)
 
 // 初始化所有任务
 import { tasks } from '@/api/tasks.js'
 const allTasks = ref(tasks)
 
+// // 初始化所有任务
+// const allTasks = ref([])
+
+// // 使用 axios 请求后端接口
+// axios
+//   .get('http://localhost:8080/showTodos')
+//   .then((response) => {
+//     allTasks.value = response.data
+//   })
+//   .catch((error) => {
+//     console.error('获取任务列表失败:', error)
+//   })
+
 // 监听 props 变化并过滤任务
 const filteredTasks = ref([])
 watch([() => props.year, () => props.month, () => props.day], ([newYear, newMonth, newDay]) => {
-  filteredTasks.value = allTasks.value.filter(task => {
+  filteredTasks.value = allTasks.value.filter((task) => {
     const taskDate = new Date(task.expectedCompletionDate)
     const taskYear = taskDate.getFullYear()
     const taskMonth = taskDate.getMonth() + 1
     const taskDay = taskDate.getDate()
     return taskYear === newYear && taskMonth === newMonth && taskDay === newDay
   })
-  console.log("点击日期：", filteredTasks.value)
+  console.log('点击日期：', filteredTasks.value)
 })
 
 // 添加 tasks 的相关数据
@@ -70,14 +89,25 @@ const addCalendarEvent = () => {
     isImportant: false,
     isUrgent: false,
   }
+
   allTasks.value.push(newTask)
+
+  // // 使用 axios 发送 POST 请求
+  // axios
+  //   .post('http://localhost:8080/save', newTask)
+  //   .then((response) => {
+  //     console.log('任务保存成功:', response.data)
+  //   })
+  //   .catch((error) => {
+  //     console.error('任务保存失败:', error)
+  //   })
 
   // 清空输入框
   calendarDescription.value = ''
   calendarType.value = 'Work'
 
   // 重新过滤任务
-  filteredTasks.value = allTasks.value.filter(task => {
+  filteredTasks.value = allTasks.value.filter((task) => {
     const taskDate = new Date(task.expectedCompletionDate)
     const taskYear = taskDate.getFullYear()
     const taskMonth = taskDate.getMonth() + 1
